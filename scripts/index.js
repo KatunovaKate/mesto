cebffc1d1c009166e88a82fd1c43f53261acb68a
Kate Katunova <rfneyjdfrfnz@yandex.ru>
2021-02-27 15:43:59 +0300

    исправлены основные ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,15 @@
 let popup = document.querySelector('.popup');
+let formPopup = document.querySelector('.popup__form');
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
-let saveButton = document.querySelector('.popup__save-button');
 let userName = document.querySelector('.profile__user-name');
 let description = document.querySelector('.profile__user-description');
 let formUserName = document.querySelector('.popup__form-name');
 let formDescription = document.querySelector('.popup__form-description');
 
 function addPopup() {
+    formUserName.value = userName.textContent;
+    formDescription.value = description.textContent;
     popup.classList.add('popup_opened');
 }
 editButton.addEventListener('click', addPopup);
@@ -17,10 +19,12 @@ function closePopup() {
 }
 closeButton.addEventListener('click', closePopup);
 
-saveButton.addEventListener('click', closePopup);
-saveButton.addEventListener('click', function() {
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
     userName.textContent = formUserName.value;
     description.textContent = formDescription.value;
-})
+    closePopup()
+}
+formPopup.addEventListener('submit', formSubmitHandler);
 
 
